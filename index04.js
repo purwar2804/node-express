dcf10928386bd3d4571a9a8aabe6fa4aed8044e0
purwar2804 <utkarshpurwar2804@gmail.com>
2020-04-29 11:30:36 +0530

    Express Example

---
+++ index04.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const http=require('http');
+const hostname='localhost';
+const port=8000;
+const app=express();
+app.use((req,res,next)=>{
+	console.log(req.headers);
+	res.statusCode=200;
+	res.setHeader=('Content-Type','text/html');
+	res.end('<html><body><h1>This is express server</h1></body></html>')
+});
+const server=http.createServer(app);
+app.listen(port,hostname,() =>{
+	console.log(`server running at http://${hostname}:${port}`);
+});
