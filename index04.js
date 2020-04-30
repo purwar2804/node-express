483eaf033049fefa7a3a94bbe61aff0b4a49d401
purwar2804 <utkarshpurwar2804@gmail.com>
2020-04-30 17:38:09 +0530

    simple REST example

--- index04.js
+++ index04.js
@@ -1,10 +1,50 @@
 const express=require('express');
 const http=require('http');
+const bodyParser=require('body-parser');
 const morgan=require('morgan');
 const hostname='localhost';
 const port=8000;
 const app=express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes',(req,res,next) =>{
+	res.statusCode=200;
+	res.setHeader('Content-Type','text/plain');
+	next();
+});
+app.get('/dishes',(req,res,next) =>{
+	res.end('will send all the dishes to you');
+});
+app.post('/dishes',(req,res,next) =>{
+	res.end('will add the dishes '+req.body.name+' with details '+req.body.description);
+});
+app.put('/dishes',(req,res,next) =>{
+	res.statusCode=403;
+	res.end('put operation not supported');
+});
+app.delete('/dishes',(req,res,next) =>{
+	res.end('delete all the dishes');
+});
+
+
+app.get('/dishes/:dishId',(req,res,next) =>{
+	res.end('will send all the dish:'+req.params.dishId+'to you');
+})
+app.post('/dishes/:dishId',(req,res,next) =>{
+	res.statusCode=403;
+	res.end('post operation not supported /dishes/'+req.params.dishId)
+})
+app.put('/dishes/:dishId',(req,res,next) =>{
+	res.write('updating the dish'+req.params.dishId)
+	res.end('will update the dish'+req.body.name+'with details'+req.body.description);
+})
+app.delete('/dishes/:dishId',(req,res,next) =>{
+	res.end('deleting dish'+req.params.dishId);
+})
+
+
+
 app.use(express.static(__dirname+'/public'));
 app.use((req,res,next)=>{
 	console.log(req.headers);
@@ -13,6 +53,6 @@ app.use((req,res,next)=>{
 	res.end('<html><body><h1>This is express server</h1></body></html>')
 });
 const server=http.createServer(app);
-app.listen(port,hostname,() =>{
+server.listen(port,hostname,() =>{
 	console.log(`server running at http://${hostname}:${port}`);
 });
